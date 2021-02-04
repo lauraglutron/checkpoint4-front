757029edc83e7aab148582a0b8e5357bd6895bad
Laura Glutron <lauraglutron@MacBook-Pro.lan>
2021-02-04 16:55:42 +0100

    added carousel in WilderBookProject page

--- src/Pages/WilderBookProject.jsx
+++ src/Pages/WilderBookProject.jsx
@@ -1,6 +1,41 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Col, Card, CardTitle, CardText } from "reactstrap";
+import {
+  Row,
+  Col,
+  Card,
+  CardTitle,
+  CardText,
+  Spinner,
+  UncontrolledCarousel,
+} from "reactstrap";
+
+const items = [
+  {
+    src:
+      "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
+    altText: "Slide 1",
+    caption: "Slide 1",
+    header: "Slide 1 Header",
+    key: "1",
+  },
+  {
+    src:
+      "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
+    altText: "Slide 2",
+    caption: "Slide 2",
+    header: "Slide 2 Header",
+    key: "2",
+  },
+  {
+    src:
+      "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
+    altText: "Slide 3",
+    caption: "Slide 3",
+    header: "Slide 3 Header",
+    key: "3",
+  },
+];
 
 const WilderBookProject = (props) => {
   const [projects, setProjects] = useState([]);
@@ -19,31 +54,33 @@ const WilderBookProject = (props) => {
       });
   }, []);
 
-  if (loading) {
+  if (loading)
     return (
-      <div>
-        <span>Loading...</span>
-      </div>
+      <Col className="text-center">
+        <Spinner size="xl" color="info" />
+      </Col>
     );
-  }
 
   return (
     <>
       <div>
-        <h1>WILDER BOOK</h1>
+        <h1>Wilder Book</h1>
       </div>
-      <Col
-        xs={{ size: 10, offset: 1 }}
-        md={{ size: 10, offset: 1 }}
-        className="py-1"
-      >
-        <Card body outline color="info" className="text-center">
-          <CardTitle>{projects[0].title}</CardTitle>
-          <CardText>{projects[0].date}</CardText>
-          <CardText>{projects[0].link}</CardText>
-          <CardText>{projects[0].description}</CardText>
-        </Card>
-      </Col>
+      <Row className="m-5">
+        <Col sm={6}>
+          <UncontrolledCarousel items={items} />
+        </Col>
+        <Col sm={6}>
+          <Card body outline color="info" className="text-center">
+            <CardTitle>{projects[0].title}</CardTitle>
+            <CardText>{projects[0].date}</CardText>
+            <CardText>{projects[0].link}</CardText>
+            <CardText>{projects[0].description}</CardText>
+            <CardText>{projects[0].maintech}</CardText>
+            <CardText>{projects[0].id_clients}</CardText>
+          </Card>
+        </Col>
+      </Row>
     </>
   );
 };
